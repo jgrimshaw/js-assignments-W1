67fc739e9c5a5ce2071d60cf1d57a97d50005704
Jelena Grimshaw <41272899+jgrimshaw@users.noreply.github.com>
2019-05-02 13:24:01 -0600

    refactor with forEach / second assignmet

--- find-waldo.js
+++ find-waldo.js
@@ -11,3 +11,17 @@ function actionWhenFound(index) {
   console.log(`Found Waldo at index ${index}!`)
 }
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound)
+
+
+// second assignment
+function findWaldo(arr, actionWhenFound){
+  arr.forEach(function(elm, index){
+        if (elm === "Waldo") {
+      actionWhenFound(index);
+    }
+  })
+}
+function actionWhenFound(index) {
+  console.log(`Found Waldo at index ${index}!`)
+}
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound)
